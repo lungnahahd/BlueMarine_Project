2f312eb2d7b3a1f250b96837d2a188f219f0ee37
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-08-16 18:15:54 +0900

    Squashed commit of the following:
    commit 18f7f87680a754d2f8a835c823385eadf817486c
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 14:25:14 2021 +0900
    
        Feat : "Scene 전환"
    
        도입 Scene에서 Main Scnene으로 전환 구현
    
    commit 3992419fce1684c79ae0ec3eb867a056f98e5c63
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 13:26:08 2021 +0900
    
        Feat : "First Scene"
    
        Import House Asset
        도입 Scene 구현
    
    commit afc3431b2fdd5f6c86238a6c28988ae2919af1d1
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 03:08:59 2021 +0900
    
        Feat : "Picture Three"
    
        마지막 이미지 등장 구현
    
    commit 0061c1ee8372249ed30b183672e70a03d7d059ae
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 02:58:27 2021 +0900
    
        Feat : "VideoTwo"
    
        Video Two 시청 구현
    
    commit b64e8d068a6eaf4c0bf8d13041ac6e5647152819
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 02:38:33 2021 +0900
    
        Feat : "VideoOne"
    
        VideoOne 영상 시청 구현
    
    commit 4bb47fa975448c964423f29e0b3e8b30a3048543
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 01:52:56 2021 +0900
    
        Fix : "Controller"
    
        Main Cammera Controller 관련 문제 수정
    
    commit 275691a567ed4209345ee4e2c4437aa93d227902
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Mon Aug 16 01:28:55 2021 +0900
    
        Feat : "Scene 추가"
    
        세번째 Scene 추가 및 애니메이션 부여
    
    commit be38d2a086e095acea04f4238eeb82b3abf072e2
    Author: lungnahahd <67555400+lungnahahd@users.noreply.github.com>
    Date:   Sun Aug 15 17:55:43 2021 +0900
    
        Feat : Prefab 생성
    
        VR Cammera Prefab 생성

---
+++ BlueMarine_Project/Assets/Raycast_In_First.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Raycast_In_First : MonoBehaviour
+{
+    public GameObject cam;
+    public Image pointer;
+    float timeElapsed;
+
+    public GameObject TV;
+    public GameObject Player;
+
+
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        raticle();
+    }
+
+    void raticle()
+    {
+        RaycastHit hit;
+        Vector3 forward = cam.transform.TransformDirection(Vector3.forward * 100);
+        if (Physics.Raycast(cam.transform.position, forward, out hit))
+        {
+            if (hit.transform.tag == "TV")
+            {
+
+                pointer.fillAmount = timeElapsed / 2;
+                timeElapsed = timeElapsed + Time.deltaTime;
+
+                if (timeElapsed >= 2)
+                {
+                    StartCoroutine(ShowTV());
+                }
+            }
+
+        }
+        else
+        {
+            timeElapsed = 0;
+            pointer.fillAmount = 0;
+        }
+        Debug.DrawRay(cam.transform.position, forward, Color.red);
+    }
+    IEnumerator ShowTV()
+    {
+        Player. transform.GetComponent<Animator>().SetBool("SeeTV", true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Demo Scene");
+        
+    }
+}
+
